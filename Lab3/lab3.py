902e466610539f9adf19a55a7311f5d31033bf2a
saskarinejad-jahromi <136478677+saskarinejad-jahromi@users.noreply.github.com>
2023-06-13 21:25:27 -0400

    Update lab3.py

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -1,1 +1,9 @@
+def write_name_file():
+    name = "Shayan Askarinejad-jahromi"
+    file_path = "/home/saskarinejad-jahormi/Desktop/file.txt"
+
+    with open(file_path, "w") as file:
+        file.write(name)
+
+write_name_file()
 
