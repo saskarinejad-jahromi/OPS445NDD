d501b9bf2fc76eedb6fc5ffceafa4c57ee394597
saskarinejad-jahromi <shayan.3054@gmail.com>
2023-06-13 22:51:12 -0400

    Added try-except statement for age calculation

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -1,8 +1,11 @@
 def calculate_age():
-    birth_year = int(input("Enter your birth year: "))
-    current_year = 2023 
-    age = current_year - birth_year
-    print("Your age is:", age)
+    try:
+        birth_year = int(input("Enter your birth year: "))
+        current_year = 2023
+        age = current_year - birth_year
+        print("Your age is:", age)
+    except ValueError:
+        print("Please enter a valid integer for the birth year.")
 
 calculate_age()
 
