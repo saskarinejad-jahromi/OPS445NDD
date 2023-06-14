72c015e94a9e868a6e8791eac0d2c9347227161d
saskarinejad-jahromi <136478677+saskarinejad-jahromi@users.noreply.github.com>
2023-06-13 21:18:13 -0400

    Create lab1.py

---
+++ Lab1/lab1.py
@@ -0,0 +1,7 @@
+def calculate_age():
+    birth_year = int(input("Enter your birth year: "))
+    current_year = 2023  # Replace this with the current year if you're not using a fixed value
+    age = current_year - birth_year
+    print("Your age is:", age)
+
+calculate_age()
